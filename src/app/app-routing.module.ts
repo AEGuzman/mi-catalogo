77c59516f4b4160f16735a26f19fd83b1b5342e2
AEGuzman <65552553+AEGuzman@users.noreply.github.com>
2020-05-31 17:44:23 -0500

    E3 A5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { componentFactoryName } from '@angular/compiler';
+import { ListComponent } from './list/list.component';
+import { TableComponent } from './table/table.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: 'list',
+    component: ListComponent
+  },
+  {
+    path: 'table',
+    component: TableComponent
+
+  }  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
